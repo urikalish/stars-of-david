077c310e1f4efd64b148bea7647df35dbd5d54c7
Uri Kalish <uri.kalish@gmail.com>
2016-11-05 14:23:46 +0200

    intro delays

--- js/main-ctrl.js
+++ js/main-ctrl.js
@@ -3,6 +3,27 @@ angular.module('mainApp').controller('mainCtrl', function mainCtrl($scope, $docu
   var i;
   var introText = 'Stars of David';
   
+  function handleInto() {
+    var elm;
+    for (i = 0; i < introText.length; i++) {
+      elm = $document[0].getElementById('intro-letter-' + i);
+      elm.style['transition'] = '5s ease all ' + (i*100) + 'ms';
+      elm.style['opacity'] = '1';
+    }
+    for (i = 0; i < 3; i++) {
+      elm = $document[0].getElementById('intro-star-' + i);
+      elm.style['transition'] = '5s ease all ' + (1500 + i*250) + 'ms';
+      elm.style['opacity'] = '1';
+    }
+    $timeout(function() {
+      $document[0].getElementById('intro-panel').style['opacity'] = '0';
+      $timeout(function() {
+        $document[0].getElementById('intro-panel').style['display'] = 'none';
+        $document[0].getElementById('main-panel').style['opacity'] = 1;
+      }, 1000);
+    }, 6500);  
+  }
+
   $scope.model = {
     introTextLetters : [],
   	headerTitle: 'Stars of David',
@@ -20,25 +41,7 @@ angular.module('mainApp').controller('mainCtrl', function mainCtrl($scope, $docu
   $scope.model.items = mainServ.getItems();
 
   $timeout(function() {
-    var elm;
-    for (i = 0; i < introText.length; i++) {
-      elm = $document[0].getElementById('intro-letter-' + i);
-      elm.style['transition'] = '5s ease all ' + (i*100) + 'ms';
-      elm.style['opacity'] = '1';
-    }
-    for (i = 0; i < 3; i++) {
-      elm = $document[0].getElementById('intro-star-' + i);
-      elm.style['transition'] = '5s ease all ' + (1500 + i*250) + 'ms';
-      elm.style['opacity'] = '1';
-    }
-  }, 2500);
-
-  $timeout(function() {
-    $document[0].getElementById('intro-panel').style['opacity'] = '0';
-    $timeout(function() {
-      $document[0].getElementById('intro-panel').style['display'] = 'none';
-      $document[0].getElementById('main-panel').style['opacity'] = 1;
-    }, 1000);
-  }, 9000);
+    handleInto();
+  }, 5000);
 
 });
